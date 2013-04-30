f9790f28289e34c2d67ebb90938fd63393f41198
Bananana <s.cartmell1@gmail.com>
2013-04-30 19:34:53 +0100

    Fixed indentation, added comments

--- src/server/XMLReaderTest.java
+++ src/server/XMLReaderTest.java
@@ -9,36 +9,46 @@ import org.junit.Test;
 
 public class XMLReaderTest {
 	private XMLReader reader;
-	private Server	server;
+	private Server	  server;
+
 	private List<VideoFile> videoList, serverList;
+	
 	@Before
-	public void setUp() throws Exception {
-	reader = new XMLReader("videoList.xml");
-	videoList = reader.getList();
+	public void setUp() throws Exception 
+	{
+		reader = new XMLReader("videoList.xml");
+		videoList = reader.getList();
 	}
+	
 	@Test
-	public void createListOfVideos() {
-	assertTrue(videoList instanceof List);
+	public void createListOfVideos()
+	{
+		assertTrue(videoList instanceof List);
 	}
+	
 	@Test
-	public void listContainsVideoFiles() {
-	assertTrue(videoList.get(0) instanceof VideoFile);
+	public void listContainsVideoFiles() 
+	{
+		assertTrue(videoList.get(0) instanceof VideoFile);
 	}
+	
 	@Test
 	public void videoFileReturnsCorrectFields() {
-	VideoFile videoFile = videoList.get(0);
-	assertEquals("20120213a2", videoFile.getID());
-	assertEquals("Monsters Inc.", videoFile.getTitle());
-	assertEquals("monstersinc_high.mpg", videoFile.getFilename());
-	videoFile = videoList.get(1);
-	assertEquals("20120102b7", videoFile.getID());
-	assertEquals("Avengers", videoFile.getTitle());
-	assertEquals("avengers-featurehp.mp4", videoFile.getFilename());
-	// Check third video is returned
-	videoFile = videoList.get(2);
-	assertEquals("20120102b7", videoFile.getID());
-	assertEquals("Prometheus", videoFile.getTitle());
-	assertEquals("prometheus-featureukFhp.mp4", videoFile.getFilename());
+		// Check first video is returned
+		VideoFile videoFile = videoList.get(0);
+		assertEquals("20120213a2", videoFile.getID());
+		assertEquals("Monsters Inc.", videoFile.getTitle());
+		assertEquals("monstersinc_high.mpg", videoFile.getFilename());
+		// Check second video is returned
+		videoFile = videoList.get(1);
+		assertEquals("20120102b7", videoFile.getID());
+		assertEquals("Avengers", videoFile.getTitle());
+		assertEquals("avengers-featurehp.mp4", videoFile.getFilename());
+		// Check third video is returned
+		videoFile = videoList.get(2);
+		assertEquals("20120102b7", videoFile.getID());
+		assertEquals("Prometheus", videoFile.getTitle());
+		assertEquals("prometheus-featureukFhp.mp4", videoFile.getFilename());
 	}
 	
 	@Test
@@ -46,14 +56,18 @@ public class XMLReaderTest {
 		server = new Server();
 		serverList = server.getList();
 		assertTrue(serverList instanceof List);
+		
+		// Check first video is returned to server
 		VideoFile videoFile = serverList.get(0);
 		assertEquals("20120213a2", videoFile.getID());
 		assertEquals("Monsters Inc.", videoFile.getTitle());
 		assertEquals("monstersinc_high.mpg", videoFile.getFilename());
+		// Check second video is returned to server
 		videoFile = serverList.get(1);
 		assertEquals("20120102b7", videoFile.getID());
 		assertEquals("Avengers", videoFile.getTitle());
 		assertEquals("avengers-featurehp.mp4", videoFile.getFilename());
+		// Check third video is returned
 		videoFile = serverList.get(2);
 		assertEquals("20120102b7", videoFile.getID());
 		assertEquals("Prometheus", videoFile.getTitle());
