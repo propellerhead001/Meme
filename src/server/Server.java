f5e1597a35cd37475bb8eea7503bbed0fcd9fae4
propellerhead <rjm529@york.ac.uk>
2013-05-03 16:34:34 +0100

    Server Complete

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,23 +1,64 @@
 package server;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 
 public class Server {
 	private List<VideoFile> videoList;
 	private XMLReader reader;
-	
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	ObjectOutputStream outputToClient;
+	private int port = 1138;
+
 	public Server(){
 		reader = new XMLReader();
 		videoList = reader.getList("videolist.xml");
+		socketThread.start();
 	}
-	
+
 	public List<VideoFile> getList() {
 		return videoList;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		new Server();
 	}
+	Thread socketThread = new Thread("Socket") {
+		public void run() {
+			try {
+				openSocket();
+				writeListToSocket();
+				clientSocket.close();
+				serverSocket.close();
+			} catch (IOException e) {
+				System.out.println("ERROR on socket connection.");
+				e.printStackTrace();
+			}
+		}
+	};
 
+	private void openSocket() throws IOException{
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			System.out.println("Could not listen on port : " + port);
+			System.exit(-1);
+		}
+		System.out.println("Opened socket on : " + port + ", waiting for client.");
+
+		try {
+			clientSocket = serverSocket.accept();
+		} catch (IOException e) {
+			System.out.println("Could not accept client.");
+			System.exit(-1);
+		}
+		outputToClient = new ObjectOutputStream(clientSocket.getOutputStream());
+	}
+	private void writeListToSocket() throws IOException {
+		outputToClient.writeObject(videoList);
+	}
 }
