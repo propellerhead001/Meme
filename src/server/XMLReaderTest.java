d4618c01bee6e20fd57574d8d4ca6dee88025e9d
Bananana <s.cartmell1@gmail.com>
2013-05-03 18:24:38 +0100

    Fixed first lab tests and commented

--- src/server/XMLReaderTest.java
+++ src/server/XMLReaderTest.java
@@ -10,28 +10,35 @@ import org.junit.Test;
 public class XMLReaderTest {
 	private XMLReader reader;
 	private List<VideoFile> videoList;
+	
 	@Before
 	public void setUp() throws Exception {
-	reader = new XMLReader();
-	videoList = reader.getList("videoList.xml");
+		reader = new XMLReader();
+		videoList = reader.getList("videoList.xml");
 	}
 	@Test
 	public void createListOfVideos() {
-	assertTrue(videoList instanceof List);
+		assertTrue(videoList instanceof List);
 	}
 	@Test
 	public void listContainsVideoFiles() {
-	assertTrue(videoList.get(0) instanceof VideoFile);
+		assertTrue(videoList.get(0) instanceof VideoFile);
 	}
 	@Test
 	public void videoFileReturnsCorrectFields() {
-	VideoFile videoFile = videoList.get(0);
-	assertEquals("20120213a2", videoFile.getID());
-	assertNotNull("Monsters Inc.", videoFile.getTitle());
-	assertNotNull("monstersinc_high.mpg", videoFile.getFilename());
-	videoFile = videoList.get(1);
-	assertEquals("20120102b7", videoFile.getID());
-	assertNotNull("Avengers", videoFile.getTitle());
-	assertNotNull("avengers-featurehp.mp4", videoFile.getFilename());
-	}
+		// Check first video is returned
+		VideoFile videoFile = videoList.get(0);
+		assertEquals("20120213a2", videoFile.getID());
+		assertEquals("Monsters Inc.", videoFile.getTitle());
+		assertEquals("monstersinc_high.mpg", videoFile.getFilename());
+		// Check second video is returned
+		videoFile = videoList.get(1);
+		assertEquals("20120102b7", videoFile.getID());
+		assertEquals("Avengers", videoFile.getTitle());
+		assertEquals("avengers-featurehp.mp4", videoFile.getFilename());
+		// Check third video is returned
+		videoFile = videoList.get(2);
+		assertEquals("20120102b7", videoFile.getID());
+		assertEquals("Prometheus", videoFile.getTitle());
+		assertEquals("prometheus-featureukFhp.mp4", videoFile.getFilename());}
 }
